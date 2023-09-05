5947e41cf35df837c72ec48a3abec06e09d80e06
Javier <javier@staff-net-vpn-dhcp-7801.intern.ethz.ch>
2023-09-05 15:21:59 +0200

    Update

--- run.sh
+++ run.sh
@@ -3,10 +3,22 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-# Check if the script is run with sudo
+#constants
+SGRT_INSTALL_PATH="/opt/sgrt"
+
+#check if the script is run with sudo
 if [ "$EUID" -ne 0 ]; then
     echo "This script must be run with sudo. Please use 'sudo $0' to execute it."
     exit 1
 fi
 
-echo "hola!"
+#prompt the user for the installation path
+read -p "Please enter the installation path (default: $SGRT_INSTALL_PATH): " sgrt_install_path
+
+#use default path if the user didn't provide one
+if [ -z "$sgrt_install_path" ]; then
+    sgrt_install_path=$SGRT_INSTALL_PATH
+fi
+
+echo $SGRT_INSTALL_PATH
+echo $sgrt_install_path
