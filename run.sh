6f93f6804af76da9ea5986babbd61e9de1eee21a
Javier <javier@staff-net-vpn-dhcp-7202.intern.ethz.ch>
2023-09-06 16:36:37 +0200

    Update

--- run.sh
+++ run.sh
@@ -5,8 +5,11 @@ normal=$(tput sgr0)
 
 #default constants
 SGRT_INSTALL_PATH="/opt/sgrt"
-LOCAL_DRIVE_PATH="/local/home/$USER" #LOCAL_PATH="/local/home/$USER"
+
 MPICH_PATH="/opt/mpich"
+MY_DRIVERS_PATH="/local/home/"
+
+
 SHARED_DRIVE_PATH="/home/$USER"
 SGRT_PROJECTS_PATH="$SHARED_DRIVE_PATH/sgrt_projects" #MY_PROJECTS_PATH="/home/$USER/my_projects"
 ROCM_PATH="/opt/rocm"
@@ -14,34 +17,6 @@ VIVADO_DEVICES_MAX="1"
 XILINX_PLATFORMS_PATH="/opt/xilinx/platforms"
 XILINX_TOOLS_PATH="/tools/Xilinx"
 XRT_PATH="/opt/xilinx/xrt"
-
-#get username
-#username=$(getent passwd ${SUDO_UID})
-#username=${username%%:*}
-
-#echo $username
-
-#- ACAP_SERVERS_LIST
-#- CPU_SERVERS_LIST
-#- FPGA_SERVERS_LIST
-#- GPU_SERVERS_LIST
-    #- LOCAL_PATH
-    #- MPICH_PATH
-    #- MY_PROJECTS_PATH
-    #- ROCM_PATH  
-#- VIRTUALIZED_SERVERS_LIST
-    #- VIVADO_DEVICES_MAX
-#- XILINX_PLATFORMS_PATH
-#- XILINX_TOOLS_PATH
-#- XRT_PATH
-
-#check if the script is run with sudo
-#if [ "$EUID" -ne 0 ]; then
-#    echo ""
-#    echo "This script must be run with sudo. Please use 'sudo $0' to execute it."
-#    echo ""
-#    exit 1
-#fi
 
 #check if the user has sudo capabilities
 if ! sudo -n true 2>/dev/null; then
@@ -55,16 +30,8 @@ echo ""
 echo "${bold}sgrt_install${normal}"
 
 #get sgrt_install_path
-#echo ""
-#read -p "${bold}Please, enter the installation path (default: $SGRT_INSTALL_PATH):${normal} " sgrt_install_path
-#if [ -z "$sgrt_install_path" ]; then
-#    sgrt_install_path=$SGRT_INSTALL_PATH
-#fi
-
-#get sgrt_install_path
 echo ""
 echo "${bold}Please, enter the installation path (default: $SGRT_INSTALL_PATH):${normal}"
-echo ""
 while true; do
     read -p "" sgrt_install_path
     #assign to default if empty
@@ -77,29 +44,36 @@ while true; do
     fi
 done
 
-
-
-
-#check on sgrt_install_path
-if [ -d "$sgrt_install_path" ]; then
-    echo ""
-    echo "Directory '$sgrt_install_path' already exists. Exiting."
-    echo ""
-    exit 1
-fi
-
-#get local_path
+#get mpich_path
 echo ""
-read -p "${bold}Please, enter the value for LOCAL_DRIVE_PATH (default: $LOCAL_DRIVE_PATH):${normal} " local_path
-if [ -z "$local_path" ]; then
-    local_path=$LOCAL_DRIVE_PATH
-fi
+echo "${bold}Please, enter the value for MPICH_PATH (default: $MPICH_PATH):${normal}"
+while true; do
+    read -p "" mpich_path
+    #assign to default if empty
+    if [ -z "$mpich_path" ]; then
+        mpich_path=$MPICH_PATH
+    fi
+done
+
+#get my_drivers_path
+echo ""
+echo "${bold}Please, enter the value for MY_DRIVERS_PATH (default: $MY_DRIVERS_PATH):${normal}"
+while true; do
+    read -p "" my_drivers_path
+    #assign to default if empty
+    if [ -z "$my_drivers_path" ]; then
+        my_drivers_path=$MY_DRIVERS_PATH
+    fi
+done
 
 
 #test
-echo $SGRT_INSTALL_PATH
 echo $sgrt_install_path
-echo $local_path
+echo ""
+echo $mpich_path
+echo $my_drivers_path
+
+#create files
 
 
 
